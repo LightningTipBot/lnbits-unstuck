4456b0eed1356429e099fae7ce7b6d2b45864352
lngohumble <lngohumble@gmail.com>
2021-10-07 12:48:25 +0200

    add lnbits api key

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ const (
 	TelegramBotApiKey    = ""
 	LnBitsHost           = "https://lnbits.com"
 	RestartLnBitsCommand = ""
+	XApiKey              = ""
 )
 
 var TelegramUsers = []string{}
@@ -22,6 +23,9 @@ func main() {
 	if len(LnBitsHost) == 0 {
 		panic(fmt.Errorf("please provide a lnbits host"))
 	}
+	if len(XApiKey) == 0 {
+		panic(fmt.Errorf("please provide a lnbits API key"))
+	}
 	lnBitsEndpoint, err := url.Parse(fmt.Sprintf("%s/api/v1/wallet", LnBitsHost))
 	if err != nil {
 		panic(err)
@@ -29,6 +33,7 @@ func main() {
 	c := http.Client{Timeout: time.Second * 5}
 	for {
 		r := &http.Request{Header: http.Header{}, URL: lnBitsEndpoint}
+		r.Header.Set("X-API-KEY", XApiKey)
 		_, err := c.Do(r)
 		if err, ok := err.(net.Error); ok && err.Timeout() {
 			if len(TelegramBotApiKey) > 0 && len(TelegramUsers) > 0 {
